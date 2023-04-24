f229e2ea68f8a1df99db07ca84ff9b0129f0c70a
tiffyy101 <bobatiffy101@gmail.com>
2023-04-24 10:15:37 -0400

    4

--- vueAPI-project/src/views/HomeView.vue
+++ vueAPI-project/src/views/HomeView.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script setup>
-
+import { ref, onMounted } from 'vue'
+const sat = ref('')
+async function getSat() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/f9bf-2cp4.json')
+  let data = await res.json()
+  sat.value.values=data.results
+}
 </script>
 
 <style lang="scss" scoped>
