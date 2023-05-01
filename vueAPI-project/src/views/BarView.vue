880655e049edef31eb282476e53b6a5bd60d4e6b
tiffyy101 <bobatiffy101@gmail.com>
2023-05-01 10:14:53 -0400

    8

--- vueAPI-project/src/views/BarView.vue
+++ vueAPI-project/src/views/BarView.vue
@@ -1,6 +1,18 @@
 <template>
     <div class="container">
       <h1>Main Causes of Death in NY</h1>
+      <select id="filterSelect" @click="filterSelect">
+      <option value="alzheimer">Alzheimer's Disease</option>
+      <option value="hepatitis">Viral Hepatitis</option>
+      <option value="septicemia">Septicemia</option>
+      <option value="influenza">Influenza</option>
+      <option value="respirtory">Chronic Lower Respiratory Diseases</option>
+      <option value="diabetes">Diabetes Mellitus</option>
+      <option value="neoplasms">Malignant Neoplasms</option>
+      <option value="heart">Diseases of Heart</option>
+      <option value="assault">Assault</option>
+      <option value="other">Other Causes</option>
+      </select>
     </div>
   </template>
 
--- vueAPI-project/src/views/PieView.vue
+++ vueAPI-project/src/views/PieView.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="container">
       <h1>Number of Deaths Between Males and Females</h1>
+      <select id="filterSelect" @click="filterSelect">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+
     </div>
   </template>
 
