8fcf1c2185e9b7dca32c8a5f36af2b1791e0336a
tiffyy101 <bobatiffy101@gmail.com>
2023-05-02 18:02:48 -0400

    yayayayyayayay

--- vueAPI-project/src/views/HomeView.vue
+++ vueAPI-project/src/views/HomeView.vue
@@ -1,25 +1,21 @@
 <template>
-  <!-- <div class="card">
+  <div class="card">
     <infoCard v-for="data in info"
     :key="data.sex"
     :sex="data.sex"
     :year="data.year"
-    :leading_cause="data.leading_cause"/> -->
-  <!-- </div> -->
+    :leading_cause="data.leading_cause"/> 
+  </div>
 </template>
 
-<script setup>
-// import { ref, onMounted } from 'vue'
-// import infoCard from '../components/DataInfo.vue';
-// const info = ref('')
-// async function getData() {
-//   let res = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
-//   let data = await res.json()
-//   info.value=data.results
-// }
-// onMounted(() =>{
-//   getData()
-// })
+<script>
+ import infoCard from '../components/DataInfo.vue';
+ export default {
+  name: "Home",
+  components: {
+    infoCard, 
+  },
+}
 </script>
 
 <style lang="scss" scoped>
