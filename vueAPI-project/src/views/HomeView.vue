f4cada0e39ec548f0b09ed1afe0f6832dcca7e66
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-05-03 10:13:49 -0400

    13

--- vueAPI-project/src/views/HomeView.vue
+++ vueAPI-project/src/views/HomeView.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="card">
-    <infoCard v-for="data in info"
-    :key="data.sex"
-    :sex="data.sex"
-    :year="data.year"
-    :leading_cause="data.leading_cause"/> 
+  <div class="information">
+    <h1>Sex: {{ sex }}</h1>
+    <h2>Year: {{ year }}</h2>
+    <h3>Leading Cause: {{ leading_cause }}</h3>
   </div>
 </template>
 
 <script>
- import infoCard from '../components/DataInfo.vue';
- export default {
-  name: "Home",
-  components: {
-    infoCard, 
-  },
+async function res() {
+  const data = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+  const x = await response.json();
+  console.log(x);
 }
+
+
 </script>
 
 <style lang="scss" scoped>
